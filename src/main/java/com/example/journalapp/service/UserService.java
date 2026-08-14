e7b923dc259790e7c0ebec2b2915ee639cb6b5d9
Muhammadsid2011 <muhammadsid2011@gmail.com>
2026-08-14 15:52:42 +0500

    Add UserService class for user management functionality

---
+++ src/main/java/com/example/journalapp/service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.journalapp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.journalapp.entity.User;
+import com.example.journalapp.repository.UserRepository;
+
+@Component
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveEntry(User user){
+        userRepository.save(user);
+    }
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+    
+    public Optional<User> findById(ObjectId id){
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id){
+        userRepository.deleteById(id);
+    }
+
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
