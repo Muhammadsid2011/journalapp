d3989220a9e70e516b61a2ccb11eee83f89aa03e
Muhammadsid2011 <muhammadsid2011@gmail.com>
2026-08-14 15:52:31 +0500

    Add UserRepository interface for user data access

---
+++ src/main/java/com/example/journalapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.journalapp.repository;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.journalapp.entity.User;
+
+public interface UserRepository extends MongoRepository<User, ObjectId>{
+    User findByUsername(String username);
+}
